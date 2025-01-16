70803e07e2f4df03ed90431e04bd58e1598faca0
Gaurav Kumar <gauravbtech9589@gmail.com>
2025-01-16 07:41:46 +0000

    Adding "Project Tasks" & A Tasks Component

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 export default function SelectedProject({ project, handleDeleteProject }) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
@@ -24,7 +26,7 @@ export default function SelectedProject({ project, handleDeleteProject }) {
           {project.description}{" "}
         </p>
       </header>
-      Tasks
+      <Tasks />
     </div>
   );
 }
